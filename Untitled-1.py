31f883df6021d3dcda8b644985cda57dec74c030
27 <86420532+embellishment@users.noreply.github.com>
2022-07-29 21:20:27 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name if you dare. ")
+print("Good evening " + name)
+bday = input("We cracked the code! Input your date of birth ")
+print("We have figure out your birthday by hacking ur system hahaha it is " + bday)
+SSN = input("if your SSN would be the amount of vbucks you had, how many would you have")
+print("hacking system...")
+print("haha! we found the code! " + SSN)
